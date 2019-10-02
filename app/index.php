77d6819b920b7530ddd1a0c3edad4891004de610
John Cleary <john@createk.io>
2019-10-02 18:09:05 +0100

    read from the users table

--- app/index.php
+++ app/index.php
@@ -2,8 +2,25 @@
 
 echo "Hello World";
 
-$username = '';
-$password = '';
-$connection_string = '';
+$username = 'system';
+$password = 'oracle';
+$connection_string = 'oracle';
 
-oci_connect($username , $password , $connection_string);
+$conn = oci_connect($username , $password , $connection_string);
+
+$query = "select id, name from users";
+
+
+$stid = oci_parse($conn, $query);
+
+oci_execute($stid);
+echo "<table border='1'>\n";
+while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+    echo "<tr>\n";
+    foreach ($row as $item) {
+        echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+    }
+    echo "</tr>\n";
+}
+echo "</table>\n";
+
